3fdf78bb54d064c80ab75f2bdd2873bbf44140bf
saurabhhh777 <jeeaspirantwallah2022@gmail.com>
2025-03-31 17:45:18 +0530

    backend url change

--- frontend/lib/axios.jsx
+++ frontend/lib/axios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: "http://localhost:7000",
+    baseURL: `${import.meta.env.VITE_BACKEND_URL}`,
     withCredentials: true,
     headers: {
         "Content-Type": "application/json",
